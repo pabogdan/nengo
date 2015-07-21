cabdcf37f2fa705a6bee83b897b1be19392e0b43
Seanny123 <saubin@uwaterloo.ca>
2015-07-21 18:18:48 -0400

    added State to replace buffer/memory

--- nengo/spa/state.py
+++ nengo/spa/state.py
@@ -1,13 +1,12 @@
-import warnings
-
 import numpy as np
 
 import nengo
 from nengo.spa.module import Module
+from nengo.networks.ensemblearray import EnsembleArray
 
 
-class Buffer(Module):
-    """A module capable of representing a single vector, with no memory.
+class State(Module):
+    """A module capable of representing a single vector, with optional memory.
 
     This is a minimal SPA module, useful for passing data along (for example,
     visual input).
@@ -26,12 +25,12 @@ class Buffer(Module):
     direct : boolean
         Whether or not to use direct mode for the neurons
     """
+
     def __init__(self, dimensions, subdimensions=16, neurons_per_dimension=50,
+                 feedback=1.0, feedback_synapse=0, tau=0.1,
                  vocab=None, direct=False, label=None, seed=None,
                  add_to_container=None):
-        warnings.warn("Buffer is deprecated in favour of spa.State",
-                      DeprecationWarning)
-        super(Buffer, self).__init__(label, seed, add_to_container)
+        super(State, self).__init__(label, seed, add_to_container)
 
         if vocab is None:
             # use the default one for this dimensionality
@@ -49,7 +48,9 @@ class Buffer(Module):
                              'subdimensions(%d)' % (dimensions, subdimensions))
 
         with self:
-            self.state = nengo.networks.EnsembleArray(
+            self.input = nengo.Node(size_in=dimensions)
+            self.output = nengo.Node(size_in=dimensions)
+            self.state_ensembles = EnsembleArray(
                 neurons_per_dimension * subdimensions,
                 dimensions // subdimensions,
                 ens_dimensions=subdimensions,
@@ -57,5 +58,20 @@ class Buffer(Module):
                 radius=np.sqrt(float(subdimensions) / dimensions),
                 label='state')
 
-        self.inputs = dict(default=(self.state.input, vocab))
-        self.outputs = dict(default=(self.state.output, vocab))
+        self.inputs = dict(default=(self.input, vocab))
+        self.outputs = dict(default=(self.output, vocab))
+
+        with self:
+            if(feedback == 0.0):
+                nengo.Connection(self.input, self.state_ensembles.input,
+                                 synapse=None)
+            else:
+                nengo.Connection(self.input, self.state_ensembles.input,
+                                 transform=feedback,
+                                 synapse=nengo.Lowpass(tau))
+                nengo.Connection(self.state_ensembles.output,
+                                 self.state_ensembles.input,
+                                 transform=feedback, synapse=feedback_synapse)
+            
+            nengo.Connection(self.state_ensembles.output, self.output,
+                             synapse=None)
